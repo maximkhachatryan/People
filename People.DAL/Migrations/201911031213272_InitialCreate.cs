a31e365a74009f94fa9fd55ca1facfbe9e18fdf5
Maxim <maxim.khachatryan.2013@gmail.com>
2019-11-03 16:32:47 +0400

    Context configuration and migrations

---
+++ People.DAL/Migrations/201911031213272_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿namespace People.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        PID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(maxLength: 240),
+                        LastName = c.String(maxLength: 240),
+                        Notes = c.String(maxLength: 2400),
+                        Phone = c.String(maxLength: 240),
+                        Email = c.String(maxLength: 240),
+                        CrDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.PID)
+                .Index(t => t.PID, clustered: true, name: "PK_Folks");
+            Sql("ALTER TABLE dbo.People ADD CONSTRAINT DF_Folks_CrDate DEFAULT GETDATE() FOR CrDate");
+
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.People", "PK_Folks");
+            DropTable("dbo.People");
+        }
+    }
+}
---
+++ People.DAL/PeopleContext.cs
@@ -0,0 +1,47 @@
+﻿using People.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.DAL
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext()
+            : base("PeopleConnString")
+        {
+            Configuration.LazyLoadingEnabled = true;
+        }
+        public virtual DbSet<Person> Folks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasKey(x => x.Id);
+            modelBuilder.Entity<Person>().Property(x => x.Id).HasColumnName("PID");
+
+            modelBuilder.Entity<Person>().Property(x => x.FirstName)
+                .HasMaxLength(240);
+
+            modelBuilder.Entity<Person>().Property(x => x.LastName)
+                .HasMaxLength(240);
+
+            modelBuilder.Entity<Person>().Property(x => x.Notes)
+                .HasMaxLength(2400);
+
+            modelBuilder.Entity<Person>().Property(x => x.Phone)
+                .HasMaxLength(240);
+
+            modelBuilder.Entity<Person>().Property(x => x.Email)
+                .HasMaxLength(240);
+
+
+            modelBuilder.Entity<Person>().HasIndex(x => x.Id).IsClustered().HasName("PK_Folks");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
