dd30fbd58234dbbec705a12880c17d4b8a501172
Maxim <maxim.khachatryan.2013@gmail.com>
2019-11-03 18:14:58 +0400

    Implemented repository pattern.

---
+++ People.DAL/Services/IPeopleRepository.cs
@@ -0,0 +1,21 @@
+﻿using People.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.DAL.Services
+{
+    public interface IPeopleRepository<TEntity> where TEntity : EntityBase
+    {
+        void Create(TEntity item);
+        TEntity FindById(Guid id);
+        IEnumerable<TEntity> Get();
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+        bool Remove(TEntity item);
+        IEnumerable<TEntity> GetWithInclude(params Expression<Func<TEntity, object>>[] includeProperties);
+        IEnumerable<TEntity> GetWithInclude(Func<TEntity, bool> predicate, params Expression<Func<TEntity, object>>[] includeProperties);
+    }
+}
---
+++ People.DAL/Services/PeopleRepository.cs
@@ -0,0 +1,63 @@
+﻿using People.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.DAL.Services
+{
+    class PeopleRepository<TEntity> : IPeopleRepository<TEntity> where TEntity : EntityBase
+    {
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public PeopleRepository(DbSet<TEntity> dbSet)
+        {
+            _dbSet = dbSet;
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return _dbSet.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<TEntity> Get(Func<TEntity, bool> predicate)
+        {
+            return _dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+
+        public TEntity FindById(Guid id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Create(TEntity item)
+        {
+            _dbSet.Add(item);
+        }
+        public bool Remove(TEntity item)
+        {
+            bool result = _dbSet.Remove(item) != null;
+            return result;
+        }
+
+        public IEnumerable<TEntity> GetWithInclude(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            return Include(includeProperties).ToList();
+        }
+
+        public IEnumerable<TEntity> GetWithInclude(Func<TEntity, bool> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var query = Include(includeProperties);
+            return query.Where(predicate).ToList();
+        }
+
+        IQueryable<TEntity> Include(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _dbSet.AsNoTracking();
+            return includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+    }
+}
